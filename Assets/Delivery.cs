3b66360786b1ffc33706b0ea1867998fe5111c90
Ali <tamnends@gmail.com>
2024-09-23 00:30:15 +0300

    Updated

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -2,8 +2,18 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
     bool packageOn = false;
     [SerializeField] private float delayOfDestroy = 0.1f;
+
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -11,12 +21,14 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package picked up");
             packageOn = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(collision.gameObject, delayOfDestroy);
         }
 
         if (collision.tag == "Customer" && packageOn)
         {
             Debug.Log("Package picked down");
+            spriteRenderer.color = noPackageColor;
             packageOn = false;
         }
     }
